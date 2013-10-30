9f04dfcb05b05d4e0d1ca1dca73825f67a2efbcf
Lou Wolford <lou.wolford.ctr@adlnet.gov>
2013-10-30 15:03:53 -0400

    updated build time

--- xapiwrapper.js
+++ xapiwrapper.js
@@ -155,7 +155,7 @@ if ( !Date.prototype.toISOString ) {
     XAPIWrapper.prototype.xapiVersion = "1.0.1";
 
     // This wrapper was built on:
-    XAPIWrapper.prototype.build = "2013-10-28T17:08Z";
+    XAPIWrapper.prototype.build = "2013-10-30T15:00Z";
 
     /*
      * prepareStatement
