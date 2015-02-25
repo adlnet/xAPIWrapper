a6851ec0674da2804dd7b67a548f707d436a090f
Tyler Mulligan <tyler.mulligan.ctr@adlnet.gov>
2015-02-25 09:27:46 -0500

    added gh-pages task but it is questionable

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,18 +2,18 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    bump: {
+    'pkg': grunt.file.readJSON('package.json'),
+    'bump': {
       options: {
         updateConfigs: ['pkg'],
         commitFiles: ['-a']
       }
     },
-    uglify: {
+    'uglify': {
       options: {
         banner: '/*! <%= pkg.name %> v <%= pkg.version %> | Built on <%= grunt.template.today("yyyy-mm-dd HH:MM:sso") %> */\n'
       },
-      build: {
+      'build': {
         files: {
           'dist/xapiwrapper.min.js': [
             'src/cryptojs_v3.1.2.js',
@@ -24,8 +24,16 @@ module.exports = function(grunt) {
         }
       }
     },
-    exec: {
+    'exec': {
       docs: './node_modules/doxstrap/bin/doxstrap.js --title "xAPIWrapper <%= pkg.version %> Reference" --output doc'
+    },
+    'gh-pages': {
+      options: {
+        base: 'doc'
+      },
+      all: {
+        src: ['**/*']
+      }
     }
   });
 
@@ -33,9 +41,14 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-bump');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-exec');
+  grunt.loadNpmTasks('grunt-gh-pages');
 
   // Default task(s).
   grunt.registerTask('default', ['uglify','exec']);
+
+  // Push docs to gh-pages branch
+  grunt.registerTask('gh-pages', ['gh-pages']);
+
   // those with adl repo access can use this to publish a tag and release
   // $> grunt release:minor
   grunt.registerTask('release', 'Build the release of xapiwrapper', function(n) {
