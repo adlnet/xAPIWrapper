fa80f8252b55ae2c8bf84efdf062cbef4aa1b9bd
IoannaP <ioana.pandele1@gmail.com>
2015-03-16 17:38:51 +0200

    Update xapiwrapper.js
    Fix misspelling: octect -> octet

--- src/xapiwrapper.js
+++ src/xapiwrapper.js
@@ -505,7 +505,7 @@ function isDate(date) {
                 else
                 {
                     headers = headers || {};
-                    headers["Content-Type"] ="application/octect-stream";
+                    headers["Content-Type"] ="application/octet-stream";
                 }
             }
             else
