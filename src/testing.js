b07963484d16f68e687109b380658cc397a2a0ae
Andy Creighton <Andy Creighton>
2015-12-10 16:45:09 -0500

    testing to work in both browser and node

---
+++ src/testing.js
@@ -0,0 +1,8 @@
+/*This is testing out some scenarios and work arounds
+once you are done - delete this */
+
+var mymodule = require('./mymodule');
+    // sys = require('sys');
+
+console.log('hello world');
+console.log(mymodule.test());
