f6f8704e7ff276b3c240e959604157307a0f8793
tom creighton <tjcreighton@gmail.com>
2015-12-21 08:55:00 -0500

    fixing stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,7 +20,10 @@ module.exports = function(grunt) {
             'src/verbs.js',
             'src/xapiwrapper.js',
             'src/xapistatement.js',
-            'src/xapi-util.js'
+            'src/xapi-util.js',
+            'src/offline.js',
+            'src/storage.js',
+            'src/validator.js'
           ]
         }
       }
