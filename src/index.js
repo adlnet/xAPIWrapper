7945f55ca6c27bf4823450edcb10134b5c5c098a
Anderson <andersoa@vacolab.adlnet.org>
2017-04-11 10:06:01 -0400

    Fixed minified wrapper errors

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-// Create ADL namespace
-var ADL = module.exports;
+if (typeof module !== 'undefined') {
+  // Create ADL namespace
+  var ADL = module.exports;
 
-// Require all modules (resource pool for all dependencies)
-(function(){
+  // Require all modules (resource pool for all dependencies)
+  (function(){
 
-  this.Agent = require('./Agent.js').Agent;
-  this.Group = require('./Agent.js').Group;
-  this.Verb = require('./Verb.js');
-  this.verbs = require('./verbs.js');
-  this.Statement = require('./Statement.js');
-  this.Activity = require('./Object.js').Activity;
-  this.StatementRef = require('./Object.js').StatementRef;
-  this.SubStatement = require('./Object.js').SubStatement;
-  this.Util = require('./Utils.js');
-  // this.LRS = require('./LRS.js');
+    this.Agent = require('./Agent.js').Agent;
+    this.Group = require('./Agent.js').Group;
+    this.Verb = require('./Verb.js');
+    this.verbs = require('./verbs.js');
+    this.Statement = require('./Statement.js');
+    this.Activity = require('./Object.js').Activity;
+    this.StatementRef = require('./Object.js').StatementRef;
+    this.SubStatement = require('./Object.js').SubStatement;
+    this.Util = require('./Utils.js');
+    // this.LRS = require('./LRS.js');
 
-}).apply(ADL);
+  }).apply(ADL);
+}
