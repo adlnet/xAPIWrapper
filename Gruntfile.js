26b62366b423912f1b40407dcd1151f98ac7d04f
Oliver Foster <oliver.foster@kineo.com>
2019-02-18 15:31:23 +0000

    Added intellisense and sourcemapping support

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,26 @@
 module.exports = function(grunt) {
-var wrapper = grunt.file.read('./umdwrapper.js').split('//<%= output =>');
+
+  var wrapper = grunt.file.read('./umdwrapper.js').split('//<%= output =>');
+
+  var fs = require("fs");
+  var path = require("path");
+
+  (function makeStrippedTempFiles(files) {
+    if (!fs.existsSync('temp')) fs.mkdirSync('temp');
+    files.forEach((file)=>{
+      // Remove the closure wrapping each file and save in a temporary folder
+      // This allows intellisense to work correctly
+      var stripped = grunt.file.read(file).replace(/(^([^\n]+\n{1}){1})|(\n[^\n]+\n$)/g, "");
+      fs.writeFileSync(path.join('temp', path.parse(file).base), stripped);
+    });
+  })([
+    'src/activitytypes.js',
+    'src/verbs.js',
+    'src/xapiwrapper.js',
+    'src/xapistatement.js',
+    'src/xapi-util.js',
+    'src/xapi-launch.js'
+  ]);
 
   // Project configuration.
   grunt.initConfig({
@@ -11,30 +32,50 @@ var wrapper = grunt.file.read('./umdwrapper.js').split('//<%= output =>');
       }
     },
     'uglify': {
+      options: {
+        banner: '/*! <%= pkg.name %> v <%= pkg.version %> | Built on <%= grunt.template.today("yyyy-mm-dd HH:MM:sso") %> */\n'+wrapper[0],
+        footer: wrapper[1]
+      },
+      'development': {
+        options: {
+          mangle: false,
+          output: {
+              comments: 'all',
+              beautify: true
+          },
+          files: {
+            'dist/xapiwrapper.js': [
+              'lib/cryptojs_v3.1.2.js',
+              'lib/utf8-encoding.js',
+              'temp/activitytypes.js',
+              'temp/verbs.js',
+              'temp/xapiwrapper.js',
+              'temp/xapistatement.js',
+              'temp/xapi-util.js',
+              'temp/xapi-launch.js'
+            ]
+          }
+        }
+      },
       'build': {
         options: {
-          banner: '/*! <%= pkg.name %> v <%= pkg.version %> | Built on <%= grunt.template.today("yyyy-mm-dd HH:MM:sso") %> */\n'+wrapper[0],
-          footer: wrapper[1],
-          // sourceMap: {
-          //     filename: 'xapiwrapper.min.js.map',
-          //     url: 'xapiwrapper.min.js.map',
-          //     root: 'dist/',
-          //     includeSources: true
-          // },
-          // output: {
-          //     beautify: true
-          // }
+          sourceMap: {
+              filename: 'xapiwrapper.min.js.map',
+              url: 'xapiwrapper.min.js.map',
+              root: 'dist/',
+              includeSources: true
+          }
         },
         files: {
-          'dist/xapiwrapper.min.js': [
+          'dist/xapiwrapper.js': [
             'lib/cryptojs_v3.1.2.js',
             'lib/utf8-encoding.js',
-            'src/activitytypes.js',
-            'src/verbs.js',
-            'src/xapiwrapper.js',
-            'src/xapistatement.js',
-            'src/xapi-util.js',
-            'src/xapi-launch.js'
+            'temp/activitytypes.js',
+            'temp/verbs.js',
+            'temp/xapiwrapper.js',
+            'temp/xapistatement.js',
+            'temp/xapi-util.js',
+            'temp/xapi-launch.js'
           ]
         }
       }
