2dd9ad8e6524cac7fd2cf06fa9462641ebdf608b
Oliver foster <oliver.foster@kineo.com>
2019-03-29 12:47:39 +0000

    issue/142-browser: revert spacing for Date.prototype.toISOString

--- test/my.mock.wrapper.js
+++ test/my.mock.wrapper.js
@@ -49,28 +49,28 @@
 // adds toISOString to date objects if not there
 // from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toISOString
 if ( !Date.prototype.toISOString ) {
-    ( function() {
+  ( function() {
 
-      function pad(number) {
-        var r = String(number);
-        if ( r.length === 1 ) {
-          r = '0' + r;
-        }
-        return r;
+    function pad(number) {
+      var r = String(number);
+      if ( r.length === 1 ) {
+        r = '0' + r;
       }
+      return r;
+    }
 
-      Date.prototype.toISOString = function() {
-        return this.getUTCFullYear()
-          + '-' + pad( this.getUTCMonth() + 1 )
-          + '-' + pad( this.getUTCDate() )
-          + 'T' + pad( this.getUTCHours() )
-          + ':' + pad( this.getUTCMinutes() )
-          + ':' + pad( this.getUTCSeconds() )
-          + '.' + String( (this.getUTCMilliseconds()/1000).toFixed(3) ).slice( 2, 5 )
-          + 'Z';
-      };
+    Date.prototype.toISOString = function() {
+      return this.getUTCFullYear()
+        + '-' + pad( this.getUTCMonth() + 1 )
+        + '-' + pad( this.getUTCDate() )
+        + 'T' + pad( this.getUTCHours() )
+        + ':' + pad( this.getUTCMinutes() )
+        + ':' + pad( this.getUTCSeconds() )
+        + '.' + String( (this.getUTCMilliseconds()/1000).toFixed(3) ).slice( 2, 5 )
+        + 'Z';
+    };
 
-    }() );
+  }() );
 }
 
 // shim for old-style Base64 lib
